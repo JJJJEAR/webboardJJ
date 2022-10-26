3ed9f65f0dbb925e271959bca31ad5a0738fc177
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2022-10-26 12:08:36 +0700

    w6-7

--- register.php
+++ register.php
@@ -11,25 +11,83 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <title>Document</title>
 </head>
 <body>
-    <div>
-        <H1 style="font-size: 200%"><center><b> สมัครสมาชิก </b></center></H1>
-        <hr>
-    <table style="border: 2px solid black; width : 40%" align="center">
-        <tr><td style="background-color:#6CD2FE" colspan="2" size="50">กรอกข้อมูล</td></tr>
-         <tr><td>ชื่อบัญชี:</td><td><input type="text" name ="first name" size="30"></td></tr><br>
-         <tr><td>รหัสผ่าน:</td><td><input type="text" name ="Lastname" size="30"></td></tr><br> 
-         <tr><td>ชื่อ-นามสกุล:</td><td><input type="text" name ="Full name" size="30"></td></tr><br>
-         <tr><td></td><td><input type="radio" name="Male" value="M"> ชาย</td></tr>
-         <tr><td>เพศ:</td><td><input type="radio" name="Female" value="F"> หญิง</td></tr>
-         <tr><td></td><td><input type="radio" name="other" value="O"> อื่นๆ</td></tr>
-         <tr><td>อีเมล:</td><td><input type="text" name ="Email" size="30"></td></tr><br> 
-         <tr><td colspan="2" align="center"><input type="submit" value="สมัครสมาชิก" ></td></tr>
-    </table>
+    <div class="container">
+    <H1 style="font-size: 200%"><center><b> WebBoard JJ </b></center></H1>
+    <?php include "nav.php";?>
     <br>
-    <div align = "center"><a href="index.php" > กลับไปยังหน้าหลัก </a> </div>
-</div> 
+    
+    <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
+        <div class="card text-dark bg-white border-primary">
+            <div class="card-header bg-primary text-white">กรอกข้อมูล</div>
+            <div class="card-body">
+                <form action="register_save.php" method="post">
+                    <div class="row mb-3">
+                        <label class="col-md-3 col-form-label">ชื่อบัญชี :</label>
+                        <div class="col-md-9">
+                            <input type="text" name="login" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-md-3 col-form-label">รหัสผ่าน :</label>
+                        <div class="col-md-9">
+                            <input type="password" name="pwd" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-md-3 col-form-label">ชื่อ-นามสกุล :</label>
+                        <div class="col-md-9">
+                            <input type="text" name="name" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-md-3 col-form-label pt-0">เพศ :</label>
+                        <div class="col-md-9">
+                            <div class="form-check">
+                                <input type="radio" name="gender" value="m" class="form-check-input" required>
+                                <label class="form-check-label">ชาย</label>  
+                            </div>
+                            <div class="form-check">
+                                <input type="radio" name="gender" value="f" class="form-check-input" required>
+                                <label class="form-check-label">หญิง</label>
+                            </div> 
+                            <div class="form-check">
+                                <input type="radio" name="gender" value="o" class="form-check-input" required>
+                                <label class="form-check-label">อื่นๆ</label>
+                            </div>    
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-md-3 col-form-label">อีเมล :</label>
+                        <div class="col-md-9">
+                            <input type="text" name="email" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3 ">
+                        <label class="col-md-3 col-form-label"></label>
+                        <div class="col-md-9">
+                            <button class="btn btn-primary btn-sm" type="submit">
+                            <i class="bi bi-save me-1"></i>สมัครสมาชิก</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3"></div>
+    </div>
+</div>  
+    <br>
+    <div align ="center"><a href="index.php">กลับไปหน้าหลัก</a></div>
+        
 </body>
 </html>
---
+++ register_save.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+$login = $_POST['login'];
+$passwd = $_POST['pwd'];
+$name = $_POST['name'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+
+$conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+$passwd=sha1($passwd);
+
+$sql = "SELECT * FROM user WHERE login='$login'";
+$result=$conn->query($sql);
+if($result->rowCount()==1){
+    $_SESSION['add_login']='error';
+}else{
+    $sql1="INSERT INTO user(login,password,name,gender,email,role) VALUES ('$login','$passwd','$name','$gender','$email','m')";
+    $conn->exec($sql1);
+    $_SESSION['add_login']='success';
+}
+$conn = null;
+header("location:register.php");
+die();
+?>
