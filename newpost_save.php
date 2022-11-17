b4b674861124e08c62c88e4c3df365f17bbbdf33
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2022-11-17 22:29:55 +0700

    w9

--- newpost_save.php
+++ newpost_save.php
@@ -1,12 +1,11 @@
 <?php
-   session_start();
-   $cid = $_POST['category'];
-   $uid = $_SESSION["user_id"];
-   $title = $_POST["topic"];
-   $content = $_POST["comment"];
+    session_start();
+    $cid = $_POST['category'];
+    $uid = $_SESSION["user_id"];
+    $title = $_POST["topic"];
+    $content = $_POST["comment"];
 
-
-   $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");    
+    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");    
     $sql = "INSERT INTO `post` (`title`, `content`, `post_date`, `cat_id`, `user_id`) 
             VALUES ('$title','$content',NOW(),'$cid', '$uid')";
         $conn -> exec($sql);
